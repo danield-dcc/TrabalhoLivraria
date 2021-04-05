730b365de71ba9f022e55e1210183300d4103907
Daniel Conceição <danieldcc.colares@gmail.com>
2021-04-04 21:13:38 -0300

    :art: Reformat the code Remove os espaços, os comentarios e atualiza o código.

--- autor.ts
+++ autor.ts
@@ -25,7 +25,7 @@ export class Autor {
     }
 
     
-    //método
+    
     public pegarNome(): string {
         let nomeAutor = this.nome
         return nomeAutor
@@ -36,28 +36,18 @@ export class Autor {
 
     }
 
-    //pegar como uma string e salvar como date
     public setDatadeNascimento(dataDeNscimento: string): void {
-     
         const diaFormatoBR = dataDeNscimento
         let dia1 = diaFormatoBR.slice(0, 2)
         let mes1 = diaFormatoBR.slice(3, 5)
         let ano1 = diaFormatoBR.slice(6)
         let AnoMesDia = ano1 + "/" + mes1 + "/" + dia1
-        const data = new Date(AnoMesDia) // joga as datas no formato internacional
+        const data = new Date(AnoMesDia) 
 
-        // const ano = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(data)
-        // const mes = new Intl.DateTimeFormat('en', { month: 'numeric' }).format(data)
-        // const dia = new Intl.DateTimeFormat('en', { day: 'numeric' }).format(data)
-
-        // console.log(`Data que deve ser salva: ${data}`)
-        // console.log(`${dia}/${mes}/${ano}`)
         this._dataDeNascimento = data;
-        console.log(`Data salva na classe: ${this._dataDeNascimento}`)
-
     }
 
-    public getDataDeNascimento(): void {
+    public getDataDeNascimento():string {
         let data = this._dataDeNascimento.toLocaleDateString()
         const diaFormatoBR = data
         let posicaoDia2 = diaFormatoBR.lastIndexOf("-")
@@ -66,31 +56,6 @@ export class Autor {
         let ano2 = diaFormatoBR.slice(0, posicaoAno2)
         let mes2 = diaFormatoBR.slice(posicaoAno2 + 1, posicaoDia2)
         let AnoMesDia = dia2 + "/" + mes2 + "/" + ano2
-        console.log(`Data Formatada: ${AnoMesDia}`)
-
+        return AnoMesDia
     }
-
-
-
-    // public mostrarNomes(): void {
-    //     for (let nome of this.nome) {
-    //         console.log(`Nome do Autor: ${nome}`);
-    //     }
-    // }
-  
-
-
 }
-
-
-
-
-//date internacional: ano mes dia
-const nome = new Autor('Ana', new Date('2021-03-25'))
-nome.salvarNome('Mariana Da silva Soares')
-// console.log(nome.pegarNome())
-// console.log(nome)
-nome.setDatadeNascimento('01/03/1999')
-nome.getDataDeNascimento();
-
-//n.toLocaleString("pt-BR")
