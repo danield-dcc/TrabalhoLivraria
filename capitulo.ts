3b103c0d77ada7f9ffca88ab8c6881f76b9355cb
leonardofreiper <leonardofreiper@gmail.com>
2021-04-22 17:31:47 -0300

    :truck:Efetuada troca de variaveis e limpeza de codigo

--- capitulo.ts
+++ capitulo.ts
@@ -4,33 +4,32 @@ import { Livro } from "./livro";
 //import prompt from "prompt-sync";
 
 export class Capitulo{
+    private _titulo: string;
+    private _descricao: string;
 
-    constructor(
-        private _titulo: string,
-        private _descricao: string,
-        ){}
+    constructor(titulo: string, descricao: string){
+        this._titulo = titulo;
+        this._descricao = descricao;
+    }
+        
 
-        public get titulo(): string{
+        public get titulo():string{
             return this._titulo;
         }
 
-        public set titulo(t: string){
-            this._titulo = t;
+        public set titulo(titulo:string){
+            this._titulo = titulo;
         }
 
         public get descricao(): string{
             return this._descricao;
         }
 
-        public set descricao(d: string){
-            this._descricao = d;
+        public set descricao(descricao: string){
+            this._descricao = descricao;
         }
         
 }
-
-//let teclado = prompt();
-let capitulo = new Capitulo("Cap 2 Rosas Malvadas", "Descição Terras");
-console.log(`${capitulo.titulo}\n${capitulo.descricao}`);
 
 
 
